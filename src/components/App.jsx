a2b48ef37a5f91cc05524559d8d5ffbc13337e52
jsmolos <molosj@yahoo.com>
2024-05-11 22:40:26 +0800

    fxs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,62 +1,51 @@
-import React from 'react';
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { SharedLayout } from 'pages/SharedLayout';
 import { RegisterPage } from 'pages/RegisterPage';
 import { LoginPage } from 'pages/LoginPage';
 import { ContactsPage } from 'pages/ContactsPage';
 import { RestrictedRoute } from './RestrictedRoute/RestrictedRoute';
 import { PrivateRoute } from './PrivateRoute/PrivateRoute';
-import { HomePage } from 'pages/HomePage';
 import { useDispatch } from 'react-redux';
 import { useAuth } from '../redux/hooks/useAuth';
-import { useEffect } from 'react';
 import { refreshUser } from '../redux/auth/authOperations';
+import { HomePage } from 'pages/HomePage';
 
 export const App = () => {
   const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const { isRefreshing } = useAuth();
+  const { isLoggedIn, isRefreshing } = useAuth();
 
   useEffect(() => {
-    dispatch(refreshUser());
-    navigate('/contacts');
-  }, [dispatch, navigate]);
+    if (!isLoggedIn && !isRefreshing) {
+      dispatch(refreshUser());
+    }
+  }, [dispatch, isLoggedIn, isRefreshing]);
 
-  return isRefreshing ? (
-    <h1>Refreshing user... Please wait...</h1>
-  ) : (
-    <>
-      <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<HomePage />} />
-          <Route
-            path="/register"
-            element={
-              <RestrictedRoute
-                component={RegisterPage}
-                redirectTo="/contacts"
-              />
-            }
-          />
-          <Route
-            path="/login"
-            element={
-              <RestrictedRoute component={LoginPage} redirectTo="/contacts" />
-            }
-          />
-          <Route
-            path="/contacts"
-            element={
-              <PrivateRoute component={ContactsPage} redirectTo="/login" />
-            }
-          />
-          <Route
-            path="/logout"
-            element={<PrivateRoute component={HomePage} redirectTo="/" />}
-          />
-        </Route>
-      </Routes>
-    </>
+  if (isRefreshing) {
+    return <h1>Refreshing user... Please wait...</h1>;
+  }
+
+  return (
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<HomePage />} />
+        <Route
+          path="/register"
+          element={<RestrictedRoute component={RegisterPage} redirectTo="/contacts" />}
+        />
+        <Route
+          path="/login"
+          element={<RestrictedRoute component={LoginPage} redirectTo="/contacts" />}
+        />
+        <Route
+          path="/contacts"
+          element={<PrivateRoute component={ContactsPage} redirectTo="/login" />}
+        />
+        <Route
+          path="/logout"
+          element={<PrivateRoute component={HomePage} redirectTo="/" />}
+        />
+      </Route>
+    </Routes>
   );
 };
